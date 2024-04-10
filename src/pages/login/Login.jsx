ea4ad476505e52c430d75bd258d4838376aede38
amitdutta <duttaamit356@gmail.com>
2024-04-10 23:35:09 +0530

    create user with email

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,8 @@
 import "./login.scss"
 import React, { useState } from "react";
-
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 
 const Login = () => {
@@ -8,11 +10,31 @@ const Login = () => {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // dispatch({ type: "LOGIN", payload: user })
+        navigate("/")
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
+
+
+
+
   return (
     <div className="login">
       <form
-      //  onSubmit={handleLogin}
-      >
+       onSubmit={handleLogin}>
         <input
           type="email"
           placeholder="email"
